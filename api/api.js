b4d4f32c1f807dcfe82e74a5d373f3f73a4a6b44
waynewyang <wayne.w.yang@foxmail.com>
2019-09-16 19:06:59 +0800

    add api

---
+++ api/api.js
@@ -0,0 +1,10 @@
+const Filecoin = require('filecoin-api-client')
+
+const fc = Filecoin({
+  apiAddr: '/ip4/127.0.0.1/tcp/3453/http'
+})
+
+exports.getDefalutAddress = async function() { 
+        const addr = await fc.address.default()
+        console.log(addr)
+}; 
